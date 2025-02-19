e8139635244af22d8a9c88db479bf992a2a9c94d
Gdonzeau <gdonzeau@hotmail.com>
2025-02-19 22:19:29 +0100

    Service iCloudKit mis en place.

--- Mission Prospector/Mission Prospector/DataController.swift
+++ Mission Prospector/Mission Prospector/DataController.swift
@@ -9,13 +9,17 @@ import CoreData
 import Foundation
 
 class DataController: ObservableObject {
-    let container = NSPersistentContainer(name: "Society")
-    
+    //let container = NSPersistentContainer(name: "Society")
+    let container: NSPersistentCloudKitContainer
     init() {
+        //container = NSPersistentContainer(name: "Society")
+        container = NSPersistentCloudKitContainer(name: "Society")
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
         container.loadPersistentStores { description, error in
             if let error {
                 print("Core Data failed to load: \(error.localizedDescription)")
             }
         }
+        container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
